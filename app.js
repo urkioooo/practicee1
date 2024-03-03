eddad82765b17e50a308f291e66df71bc0eaed99
urkioooo <apolinarkio887@gmail.com>
2024-03-03 13:14:04 +0800

    my first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let nextDom = document.getElementById("next");
+  let prevDom = document.getElementById("prev");
+
+  let carouselDom = document.querySelector(".carousel");
+  let SliderDom = carouselDom.querySelector(".carousel .list");
+  let thumbnailBorderDom = document.querySelector(".carousel .listThumbnail");
+  let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+
+  thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+  let timeRunning = 2000;
+  let timeAutoNext = 15000;
+
+  nextDom.onclick = function () {
+    showSlider("next");
+  };
+
+  prevDom.onclick = function () {
+    showSlider("prev");
+  };
+  let runTimeOut;
+  let runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+
+  function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+    let thumbnailItemsDom = document.querySelectorAll(
+      ".carousel .listThumbnail .item"
+    );
+
+    if (type === "next") {
+      SliderDom.appendChild(SliderItemsDom[0]);
+      thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+      carouselDom.classList.add("next");
+    } else {
+      SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+      thumbnailBorderDom.prepend(
+        thumbnailItemsDom[thumbnailItemsDom.length - 1]
+      );
+      carouselDom.classList.add("prev");
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+      carouselDom.classList.remove("next");
+      carouselDom.classList.remove("prev");
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+      next.click();
+    }, timeAutoNext);
+  }
+});
